37793a58a1195179dff631def4215fb1b6ddcda9
noxiossy <72839483+noxiossy@users.noreply.github.com>
2022-01-17 00:04:07 +0700

    Update HWCaps.cpp

--- src/Layers/xrRender/HWCaps.cpp
+++ src/Layers/xrRender/HWCaps.cpp
@@ -4,7 +4,7 @@
 #include "hw.h"
 
 #ifndef _EDITOR
-	#include "NVAPI/nvapi.h"
+	//#include "NVAPI/nvapi.h"
 	#include "ATI/atimgpud.h"
 #endif
 
@@ -14,19 +14,19 @@ namespace
 #ifndef _EDITOR
 u32 GetNVGpuNum()
 {
-	NvLogicalGpuHandle  logicalGPUs[NVAPI_MAX_LOGICAL_GPUS];
-	NvU32               logicalGPUCount;
-	NvPhysicalGpuHandle physicalGPUs[NVAPI_MAX_PHYSICAL_GPUS];
-	NvU32               physicalGPUCount;
+	//NvLogicalGpuHandle  logicalGPUs[NVAPI_MAX_LOGICAL_GPUS];
+	//NvU32               logicalGPUCount;
+	//NvPhysicalGpuHandle physicalGPUs[NVAPI_MAX_PHYSICAL_GPUS];
+	//NvU32               physicalGPUCount;
 
 //	int result = NVAPI_OK;
 
 	int iGpuNum = 0;
 
-	NvAPI_Status	status;
-	status = NvAPI_Initialize();
+	//NvAPI_Status	status;
+	//status = NvAPI_Initialize();
 
-	if (status != NVAPI_OK)
+	/*if (status != NVAPI_OK)
 	{
 		Msg("* NVAPI is missing.");
 		return iGpuNum;
@@ -65,7 +65,7 @@ u32 GetNVGpuNum()
 		Msg	("* NVidia MGPU: %d-Way SLI detected.", iGpuNum);
 	}
 
-	return iGpuNum;
+	return iGpuNum;*/
 }
 
 u32 GetATIGpuNum()
