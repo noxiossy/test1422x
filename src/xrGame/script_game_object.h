f1d827b8f98d756a6df4fb9a607c9f47a8bf4ee4
noxiossy <72839483+noxiossy@users.noreply.github.com>
2022-01-17 15:11:48 +0700

    Update script_game_object.h

--- src/xrGame/script_game_object.h
+++ src/xrGame/script_game_object.h
@@ -323,7 +323,7 @@ public:
             bool				inv_box_can_take		(bool status);
             bool				inv_box_can_take_status	();
 
-    //передача порции информации InventoryOwner
+    //ГЇГҐГ°ГҐГ¤Г Г·Г  ГЇГ®Г°Г¶ГЁГЁ ГЁГ­ГґГ®Г°Г¬Г Г¶ГЁГЁ InventoryOwner
             bool				GiveInfoPortion		(LPCSTR info_id);
             bool				DisableInfoPortion	(LPCSTR info_id);
             void				GiveGameNews		(LPCSTR caption, LPCSTR news, LPCSTR texture_name, int delay, int show_time);
@@ -331,10 +331,10 @@ public:
 
             void				AddIconedTalkMessage_old(LPCSTR text, LPCSTR texture_name, LPCSTR templ_name) {};
             void				AddIconedTalkMessage(LPCSTR caption, LPCSTR text, LPCSTR texture_name, LPCSTR templ_name);
-    //предикаты наличия/отсутствия порции информации у персонажа
+    //ГЇГ°ГҐГ¤ГЁГЄГ ГІГ» Г­Г Г«ГЁГ·ГЁГї/Г®ГІГ±ГіГІГ±ГІГўГЁГї ГЇГ®Г°Г¶ГЁГЁ ГЁГ­ГґГ®Г°Г¬Г Г¶ГЁГЁ Гі ГЇГҐГ°Г±Г®Г­Г Г¦Г 
             bool				HasInfo				(LPCSTR info_id);
             bool				DontHasInfo			(LPCSTR info_id);
-    //работа с заданиями
+    //Г°Г ГЎГ®ГІГ  Г± Г§Г Г¤Г Г­ГЁГїГ¬ГЁ
             ETaskState			GetGameTaskState	(LPCSTR task_id);
             void				SetGameTaskState	(ETaskState state, LPCSTR task_id);
             void				GiveTaskToActor		(CGameTask* t, u32 dt, bool bCheckExisting, u32 t_timer);
@@ -950,3 +950,5 @@ extern void sell_condition	(float friend_factor, float enemy_factor);
 extern void buy_condition	(CScriptIniFile *ini_file, LPCSTR section);
 extern void buy_condition	(float friend_factor, float enemy_factor);
 extern void show_condition	(CScriptIniFile *ini_file, LPCSTR section);
+
+#include "script_game_object_impl.h" 
