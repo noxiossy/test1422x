9e90c36a3deff36562d5b1eef240fa770e0c3eef
noxiossy <72839483+noxiossy@users.noreply.github.com>
2022-01-18 03:15:55 +0700

    Update alife_storage_manager_inline.h

--- src/xrGame/alife_storage_manager_inline.h
+++ src/xrGame/alife_storage_manager_inline.h
@@ -8,7 +8,7 @@
 
 #pragma once
 
-IC	CALifeStorageManager::CALifeStorageManager			(xrServer *server, LPCSTR section) :
+CALifeStorageManager::CALifeStorageManager			(xrServer *server, LPCSTR section) :
 	inherited	(server,section)
 {
 	m_section				= section;
